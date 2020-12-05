0f495cea4371076fd2f969dcad95d2a42721908c
Hector Fabio Meza <hector.meza@appgate.com>
2020-12-05 17:24:28 -0500

    Added sample unit tests

--- src/test/java/com/masivian/roulette/RouletteApplicationTests.java
+++ src/test/java/com/masivian/roulette/RouletteApplicationTests.java
@@ -1,13 +1,40 @@
 package com.masivian.roulette;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.masivian.roulette.data.Roulette;
+import com.masivian.roulette.data.RouletteRepository;
+import com.masivian.roulette.exceptions.RouletteException;
+import com.masivian.roulette.service.RouletteService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 @SpringBootTest
 class RouletteApplicationTests {
 
+	@MockBean
+	private RouletteRepository repository;
+
+	@Autowired
+	RouletteService service;
+
 	@Test
-	void contextLoads() {
+	public void testRouletteCreation () throws RouletteException {
+		service.createNewRoulette();
+		Mockito.verify(repository).save(Mockito.any(Roulette.class));
+	}
+
+	@Test
+	public void testRouletteList () throws RouletteException {
+		Mockito.when(repository.findAll()).thenReturn(List.of(new Roulette(1l), new Roulette(2l)));
+
+		List<Roulette> roulettes = service.getRoulettes();
+		Mockito.verify(repository).findAll();
+		assert(roulettes.size() == 2);
 	}
 
 }
